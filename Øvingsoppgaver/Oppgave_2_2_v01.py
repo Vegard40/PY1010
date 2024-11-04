8f0256bbe891b1d727af1934d7a8cc9a6e668290
Vegard40 <vegardtangen@gmail.com>
2024-11-04 11:28:17 +0100

    Øvingsoppgaver

--- Øvingsoppgaver/Oppgave_2_2_v01.py
+++ Øvingsoppgaver/Oppgave_2_2_v01.py
@@ -1,5 +1,5 @@
 """
-Oppgave 2.2 Lage et program i Spyder som regner om fra F til C
+Oppgave 2.2 Lag et program i Spyder som regner om fra F til C
 Lag et program i Spyder som regner om en temperatur gitt i grader fahrenheit til grader
 celsius. Brukeren skal skrive inn fahrenheit-verdien i programmet (selve programkoden), og
 b ̊ade fahrenheit-verdien og celsius-verdien skal vises i konsollen (vha. print-funksjonen).
@@ -7,4 +7,10 @@ Finn selv fram til funksjonssammenhengen mellom grader celsius og grader fahrenh
 Bruk programmet til  ̊a regne om 0 grader farenheit til grader celsius.
 57
 """
-
+#Deklarerer F som variabel med funksjonen "input", Brukeren kan selv velge grader F
+F = float(input("Skriv inn temperatur i Farenheit:"))
+#Deklarerer C med en beregning
+C = (F - 32.0) / 1.8
+#Skriver ut verdien i C og F
+print("Temperaturen er",F,"grader Farenheit")
+print("Temperaturen er",C,"grader Celsius")
