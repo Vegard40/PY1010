d0b8e5b3318a59008df53a721c24f21e74f75fd5
Vegard40 <vegardtangen@gmail.com>
2024-11-04 11:36:04 +0100

    Øvingsoppgaver

---
+++ Øvingsoppgaver/Oppgave_2_4_01.py
@@ -0,0 +1,16 @@
+"""
+Oppgave 2.4 time-modulen
+time er en modul i Python som inneholder en rekke funksjoner. time-modulen inneholder
+bl.a. time-funksjonen og sleep-funksjonen.
+
+Oppgave a
+Finn fram til dokumentasjon av time-funksjonen og sleep-funksjonen p ̊a internett, og gi en
+kort beskrivelse av hva de to funksjonene gjør.
+
+Oppgave b
+Lag et (lite) Python-program der du bruker time og sleep (f.eks. kan programmet brukes til
+ ̊a sjekke om sleep faktisk fungerer som forventet). Tips: Koden t0 = time.time() gir
+variabelen t0 verdi lik antall sekunder som har g ̊att fra det s ̊akalte epoch-tidspunktet, som
+er definert i dokumentasjonen for time-funksjonen. Obs: For  ̊a gjøre modulen time
+tilgjengelig i dine programmer m ̊a du importere modulen
+"""
