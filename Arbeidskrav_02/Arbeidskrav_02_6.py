608250f23f5a60a58c2a3ca63aaced1ea225efdc
Vegard40 <vegardtangen@gmail.com>
2024-12-03 20:46:15 +0100

    Oppdatering

---
+++ Arbeidskrav_02/Arbeidskrav_02_6.py
@@ -0,0 +1,5 @@
+"""
+Oppg 6) Skriv en kode som plotter funksjonen 𝑓(𝑥) = −𝑥2 − 5, for x på intervallet [-10,10].
+Hint: np.linspace(-10, 10, 200) gir en array med 200 punkter jevnt fordelt på intervallet
+[-10,10].
+"""
