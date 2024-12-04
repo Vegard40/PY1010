b5a807d6239694015d08bcabfd9582759b63cf59
Vegard40 <vegardtangen@gmail.com>
2024-12-04 23:29:09 +0100

    Some changes to the repo

--- Arbeidskrav_02/Arbeidskrav_02_5.py
+++ Arbeidskrav_02/Arbeidskrav_02_5.py
@@ -5,3 +5,31 @@ halvsirkel, se figuren under. Med «ytre» omkrets menes samlet lengde av de sor
 Funksjonen skal returnere arealet og «ytre» omkrets, som så skrives til skjerm med passende
 tekst.
 """
+import numpy as np
+import matplotlib.pyplot as plt
+#Ber bruker om lengde på lille og store katet
+a = int(input("Dette programmet regner ut omkrets og areal \nfor en figur satt sammen av en rettvinklet trekant og en sirkel \nskriv inn lengden for lille katet a i cm: "))
+b = int(input("Skriv inn lengden på store katet b i cm: "))
+#Regner ut hypotenus
+c = np.sqrt(a**2 + b**2)
+#Regner ut sirkelens radius
+r = a / 2.0
+#Regner ut sirkelens areal
+As = np.pi * r**2.0
+#Regner ut sirkelens omkrets
+Os = 2.0 * np.pi * r
+#Regner ut trekantens areal
+Atr = (a * b) / 2.0
+#Regner ut trekantens omkrets
+Otr = a + b + c
+#Regner ut figurens ytre omkrets
+Otot = (Os / 2.0) + a + c
+#Regner ut figurens totale areal
+Atot = (As / 2.0) + Atr
+#Skriver forespurt informasjon ut i konsollen
+#print("Arealet av sirkelen er:", As)
+#print("Omkretsen av sirkelen er: ", Os)
+#print("Arealet av trekanten er: ", Atr)
+#print("Omkretsen av trekanten er: ", Otr)
+print("Den totale omkretsen for figuren er: ", Otot, "cm")
+print("Det totale arealet for figuren er: ", Atot, "cm2")
--- Arbeidskrav_02/Arbeidskrav_02_6.py
+++ Arbeidskrav_02/Arbeidskrav_02_6.py
@@ -3,3 +3,31 @@ Oppg 6) Skriv en kode som plotter funksjonen 𝑓(𝑥) = −𝑥2 − 5, for x 
 Hint: np.linspace(-10, 10, 200) gir en array med 200 punkter jevnt fordelt på intervallet
 [-10,10].
 """
+#Importerer biblioteker som skal brukes
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Definerer x som en array med np.linspace
+x = np.linspace(-10,10,200)
+
+#Definerer funksjonen som skal plottes
+def f(x):
+    return (-x*2) - 5
+
+#Regner ut punktene med alle definerte verdier for x
+y = f(x)
+
+#Plotter linjen
+plt.plot(x, y, label= "f(x)")
+#Legger til en tittel i plottet
+plt.title("Plott av funksjonen f(x)= -2x -5")
+#Legger til verdier på x aksen
+plt.xlabel("x")
+#Legger til verdier på y aksen
+plt.ylabel("f(x)")
+#legger til rutenett i plottet
+plt.grid(True)
+#Legger til grafens "label" i plottet
+plt.legend()
+#Viser plottet
+plt.show()
