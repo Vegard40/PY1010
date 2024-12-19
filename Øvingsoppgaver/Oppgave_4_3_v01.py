f111864a8f245ba69260a0818836fa776058fee8
Vegard40 <vegardtangen@gmail.com>
2024-12-19 22:48:15 +0100

    Some changes.

--- Øvingsoppgaver/Oppgave_4_3_v01.py
+++ Øvingsoppgaver/Oppgave_4_3_v01.py
@@ -11,22 +11,34 @@ x = np.array([1, 2, 3, 4, 5])
 y = x
 z = np.sqrt(x)
 
-plt.close('all')
-plt.figure(1)
+#Figur med subplot
+#plt.subplot(figsize=(10, 6))
+
+#Subplot 1
+plt.subplot(2, 1, 1)
 plt.plot(y, x, linestyle="-", color="red", label='y vs x')
 plt.scatter(x, y, marker="*", color='blue')
-plt.plot(x, z, linestyle="-", color="blue", label='x vs z')
-plt.scatter(x, z, marker="o", color='blue')
-plt.title('x vs y, og x vs z')
+plt.title('x vs y')
 plt.legend()
 plt.grid()
 plt.xlabel("x[s]")
 plt.ylabel('[m]')
+plt.xlim(0, 6)
+plt.ylim(0, 6)
 
-plt.xlim(0, 12)
-plt.ylim(0, 9)
+plt.subplot(2, 1, 2)
+plt.plot(x, z, linestyle="-", color="blue", label='x vs z')
+plt.scatter(x, z, marker="o", color='blue')
+plt.title('x vs z')
+plt.legend()
+plt.grid()
+plt.xlabel("x[s]")
+plt.ylabel('[m]')
+plt.xlim(0, 6)
+plt.ylim(0, 6)
+
 #Genererer en .pdf fil av plottet
-plt.savefig("kurve.pdf", format="pdf")
+plt.savefig("plott2.pdf", format="pdf")
 
 plt.show()
 plt.close()
